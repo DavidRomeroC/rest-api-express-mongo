60ea06a0f0c55bc2e8d8d7742a607fc2328e12aa
DavidRomeroC <romerocandelariodavid@gmail.com>
2022-12-24 21:54:21 -0600

    middleware cors agregado para uso compartido de la api con otras aplicaciones

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const tasksRouter = require('./routes/tasks.routes')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 //MIDDLEWARES
 
 app.use(morgan('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(tasksRouter)
 
